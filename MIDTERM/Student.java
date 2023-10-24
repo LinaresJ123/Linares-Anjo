93ac1b3c555131457853b8e224ac44b26a0d4e08
LinaresJ123 <148180523+LinaresJ123@users.noreply.github.com>
2023-10-24 13:02:52 +0800

    Add files via upload

---
+++ MIDTERM/Student.java
@@ -0,0 +1,40 @@
+package Student;
+
+class Student {
+
+ String first_name= "";
+ String middle_name = "";
+ String last_name = ""
+ String student_suffix = "";
+ 
+ public Student() {
+   }
+  public void getFirstName;
+    return first_Name;
+  }
+  public void getMiddleName;
+    return middle_Name;
+  }
+  public void getLastName;
+    return last_Name;
+  
+  }
+  public void getSuffix;
+     return suffex;
+  }
+  
+  public void setFirstName(String Name){
+    first_name = Fname;
+  }  
+   public void setMiddleName(String Name){
+    first_name = Mname;
+  }  
+ public void setLastName(String Name){
+    first_name = Lname;
+  }
+   public void setSuffix(String Name){
+    first_name = suffix;
+  }  
+  
+
+  
